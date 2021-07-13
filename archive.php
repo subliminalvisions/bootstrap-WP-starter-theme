50d3c6125992e4464004b213477d845513d78f18
stephen harris <subliminalvisions@gmail.com>
2021-07-12 21:57:19 -0600

    tweaks to archive pg template

--- archive.php
+++ archive.php
@@ -9,43 +9,63 @@
 
 get_header();
 ?>
-
-	<main id="primary" class="site-main">
-
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
+	    	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+				<h1 class="page-title">
+
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+
+					$term_description = term_description();
+					if ( ! empty($term_description) ) :
+						printf( '<small class="taxonomy-description">%s</small>',
+						$term_description);
+					endif;
 				?>
-			</header><!-- .page-header -->
+				</h1>
+			</section>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			<div class="container">
+				<div id="primary" >
+					<main id="content" class="col-sm-8">
+				<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+						/*
+						* Include the Post-Type-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						*/
+						get_template_part( 'template-parts/content', get_post_type() );
 
-			endwhile;
+					endwhile;
 
-			the_posts_navigation();
+					the_posts_navigation();
 
-		else :
+			else :
 
 			get_template_part( 'template-parts/content', 'none' );
 
-		endif;
-		?>
+			endif;
+			?>
+					</main> <!-- #content -->
 
-	</main><!-- #main -->
+
+					<!-- sidebar  -->
+					<aside class="col-sm-4">
+					<?php 
+						get_sidebar();
+					?>
+										</aside>
+
+				</div> <!-- #primary  -->
+			</div>  <!-- .container  -->
+
 
 <?php
-get_sidebar();
 get_footer();
